26ec7bacd8a7143c380b3d652a6aa604d3532735
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-08-05 22:46:46 +0530

    selecting biggest polygon

--- app/src/main/java/com/secure/talkme/MainActivity.java
+++ app/src/main/java/com/secure/talkme/MainActivity.java
@@ -1,14 +1,17 @@
 package com.secure.talkme;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Talkme!");
     }
 }
